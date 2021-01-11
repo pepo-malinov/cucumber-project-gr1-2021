7c324f53ad1abe858416721372b4f17d52bdba4b
pepo <pepo@192.168.1.149>
2021-01-11 09:42:52 +0200

    Init project

---
+++ cucumber-project-gr1/src/test/java/uni/pu/fmi/LoginSteps.java
@@ -0,0 +1,39 @@
+package uni.pu.fmi;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginSteps {
+
+	@Given("^Потребителя отваря страницата за вход в ситстемата\\.$")
+	public void потребителя_отваря_страницата_за_вход_в_ситстемата() throws Throwable {
+
+	}
+
+	@When("^Въвежда потребителско име \"([^\"]*)\"$")
+	public void въвежда_потребителско_име(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Въвежда парола \"([^\"]*)\"$")
+	public void въвежда_парола(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Натиска върху бутона за вход в системата\\.$")
+	public void натиска_върху_бутона_за_вход_в_системата() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Вижда съобщение успех\\.$")
+	public void вижда_съобщение_успех() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+}
